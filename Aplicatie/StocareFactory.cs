568f62010a24872b634107358ab705cd808c5597
Skynight112 <skynight254@gmail.com>
2026-03-27 22:53:28 +0200

    git update pentru txt

--- Aplicatie/StocareFactory.cs
+++ Aplicatie/StocareFactory.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using Stocare; // Referință către proiectul care conține IStocareData și clasele de administrare
+﻿using Stocare;
 
 namespace Aplicatie
 {
-    // Clasa este statică deoarece nu avem nevoie să creăm obiecte "StocareFactory", 
-    // ci doar să apelăm metoda de fabricare a stocării.
     public static class StocareFactory
     {
-        // Păstrăm calea ta relativă pentru a găsi fișierul în folderul rădăcină al soluției
         private const string CALE_FISIER = @"..\..\..\..\intrebari.txt";
 
         public static IStocareData GetAdministratorStocare()
         {
-            // Factory-ul "fabrică" și returnează obiectul de tip AdministrareIntrebariFisierText,
-            // dar îl livrează sub formă de IStocareData (interfață).
-
+            // Metoda A: Stocare în fișier text (Activă)
             return new AdministrareIntrebariFisierText(CALE_FISIER);
+
+            // Metoda B: Stocare în memorie (Dezactivată - se folosește pentru teste rapide)
+            // return new AdministrareIntrebariMemorie();
         }
     }
 }
